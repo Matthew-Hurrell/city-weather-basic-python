1765c7b9829dc4ef175abbc416a3292639c2ed99
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-15 14:48:42 +0000

    Add basic python code to fetch weather data and linked weather API

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+API_KEY = "4cc9c46d7c87a9eebb5f478d2a3f415b"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]["description"]
+    print("Weather:", weather.capitalize())
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+    print("Temperature:", temperature, "celcius")
+else:
+    print("An error occurred.")
